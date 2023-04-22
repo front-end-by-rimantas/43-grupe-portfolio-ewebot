63d736b4f2cd8e91458740e2c7f5a4e69e7e1a1f
BaubasDRSK <127085633+BaubasDRSK@users.noreply.github.com>
2023-04-22 13:45:25 +0300

    testimonials styling and some animations

--- js/pages/home.js
+++ js/pages/home.js
@@ -38,27 +38,56 @@
 /* got new project: end */
 
 /* testimonials: start */
-let dotai = document.getElementsByClassName("dot");
+        // ballono aukstis
+        // const slides = document.getElementsByClassName("slide");
+        // console.log(slides);
+        // let heights = [];
+        // for (let i of slides){
+        //     heights.push(parseInt(i.offsetHeight,10))
+        // }
+        // console.log(heights);
+        // const height = Math.max(...heights);
+        // console.log(height);    
+        // document.getElementById("balloon").style.height = Math.max(heights)+"px";
+
+        // console.log(document.getElementById("balloon"));
 
 
-dotai[0].addEventListener('click', function () {
-    dotai[0].classList.add("active");
-    dotai[1].classList.remove("active");
-    dotai[2].classList.remove("active");
-   });
+        //Scrolling slides navigation dots behavior 
+        let dotai = document.getElementsByClassName("dot");
+        dotai[0].addEventListener('click', function () {
+            dotai[0].classList.add("active");
+            dotai[1].classList.remove("active");
+            dotai[2].classList.remove("active");
+        });
+        dotai[1].addEventListener('click', function () {
+            dotai[1].classList.add("active");
+            dotai[0].classList.remove("active");
+            dotai[2].classList.remove("active");
+        });
+        dotai[2].addEventListener('click', function () {
+            dotai[2].classList.add("active");
+            dotai[1].classList.remove("active");
+            dotai[0].classList.remove("active");
+        });
+        //dots behavior ended
 
-
-dotai[1].addEventListener('click', function () {
-    dotai[1].classList.add("active");
-    dotai[0].classList.remove("active");
-    dotai[2].classList.remove("active");
-   });
-
-dotai[2].addEventListener('click', function () {
-    dotai[2].classList.add("active");
-    dotai[1].classList.remove("active");
-    dotai[0].classList.remove("active");
-   });
+    //apearing animation
+        const observer = new IntersectionObserver(entries => {
+            // Loop over the entries
+            entries.forEach(entry => {
+              // If the element is visible
+              if (entry.isIntersecting) {
+                // Add the animation class
+                entry.target.classList.add('animation');
+              }
+            });
+          });
+          
+        // observer.observe(document.querySelector('.headers-wrapper'));
+        observer.observe(document.querySelector('.animated'));
+        observer.observe(document.querySelector('.animated2'));
+    //apearing animation end
 
   
 /* testimonials: end */
