2aee9c1992c4ef5fa5cc652b678ac9d0f7d2ba9b
BaubasDRSK <127085633+BaubasDRSK@users.noreply.github.com>
2023-04-22 20:10:24 +0300

    slider js  added

--- js/pages/home.js
+++ js/pages/home.js
@@ -38,39 +38,66 @@
 /* got new project: end */
 
 /* testimonials: start */
-        // ballono aukstis
-        // const slides = document.getElementsByClassName("slide");
-        // console.log(slides);
-        // let heights = [];
-        // for (let i of slides){
-        //     heights.push(parseInt(i.offsetHeight,10))
-        // }
-        // console.log(heights);
-        // const height = Math.max(...heights);
-        // console.log(height);    
-        // document.getElementById("balloon").style.height = Math.max(heights)+"px";
+        //balloon aukstis funkcija kad balloonas butu nemaziau negu betkurio slido auksciui
+        function changeHeight(){
+          const slides = document.getElementsByClassName("slide");
+          let heights = [];
+          for (let i of slides){
+              heights.push(parseInt(i.offsetHeight,10))
+          }
+          const height =(Math.max(...heights)+1)+'px';
+          document.getElementById("balloon").style.height = height;
+      };
 
-        // console.log(document.getElementById("balloon"));
+      //iskvieciam funkcija 
+      window.addEventListener("resize", changeHeight);
+      window.onresize = changeHeight();
 
 
-        //Scrolling slides navigation dots behavior 
-        let dotai = document.getElementsByClassName("dot");
-        dotai[0].addEventListener('click', function () {
-            dotai[0].classList.add("active");
-            dotai[1].classList.remove("active");
-            dotai[2].classList.remove("active");
-        });
-        dotai[1].addEventListener('click', function () {
-            dotai[1].classList.add("active");
-            dotai[0].classList.remove("active");
-            dotai[2].classList.remove("active");
-        });
-        dotai[2].addEventListener('click', function () {
-            dotai[2].classList.add("active");
-            dotai[1].classList.remove("active");
-            dotai[0].classList.remove("active");
-        });
-        //dots behavior ended
+      //Scrolling slides navigation dots behavior 
+      let dotai = document.getElementsByClassName("dot");
+      let slideWidth = document.getElementsByClassName("slide");
+
+      function dots(n){
+        for (let i of dotai){
+          i.classList.remove("active");
+        };
+        dotai[n].classList.add("active");
+
+        if (n===0){
+          document.getElementById("slides-container").style.transform = `translate(0px, 0px)`;
+        } else if(n===1){
+          document.getElementById("slides-container").style.transform = `translate(-${slideWidth[0].offsetWidth+20}px, 0px)`;
+        } else if (n===2){
+          document.getElementById("slides-container").style.transform = `translate(-${slideWidth[0].offsetWidth+slideWidth[1].offsetWidth+70}px, 0px)`;
+        }
+      };
+
+      function resize2(){
+        let dotai2 = document.getElementsByClassName("dot");
+        let nth = -1;
+        for (let i=0; i<dotai2.length; i++){
+          if (dotai2[i].classList.contains('active')){
+            nth = i;
+            console.log(nth);
+          }
+
+          if (nth===0){
+            document.getElementById("slides-container").style.transform = `translate(0px, 0px)`;
+          } else if(nth===1){
+            document.getElementById("slides-container").style.transform = `translate(-${slideWidth[0].offsetWidth+20}px, 0px)`;
+          } else if (nth===2){
+            document.getElementById("slides-container").style.transform = `translate(-${slideWidth[0].offsetWidth+slideWidth[1].offsetWidth+70}px, 0px)`;
+          }
+          
+        }
+
+      };
+
+      window.addEventListener("resize", resize2);
+
+
+      //dots behavior ended
 
     //apearing animation
         const observer = new IntersectionObserver(entries => {
