53dd92a187fd0d31459f0828c419846d53e04341
BaubasDRSK <127085633+BaubasDRSK@users.noreply.github.com>
2023-04-26 18:24:20 +0300

    near end fixes

---
+++ js/components/testimonials.js
@@ -0,0 +1,61 @@
+//balloon aukstis funkcija kad balloonas butu nemaziau negu betkurio slido auksciui
+function changeHeight(){
+    const slides = document.querySelectorAll(".testimonials .slide2");
+    const heights = [];
+    for (let s of slides){
+        heights.push(s.offsetHeight);
+    };
+    const height = Math.max(...heights);
+    document.querySelector('.testimonials').style.setProperty("--balloon-height", height+'px');
+    document.querySelector('.testimonials').style.setProperty("--testimonial-width", slides[0].offsetWidth);
+};
+
+const observer = new IntersectionObserver(entries => { //blokas skirtas stebeti kada pradedama animacija
+    // Loop over the entries
+    entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('animation');
+        }
+    });
+    });
+    
+
+    //dots ant slides scrolling effect
+    function slider(elem, n, dotClass, cssVariable ){
+        let sekcija = document.getElementById(n);
+        let dotai = sekcija.querySelectorAll(dotClass);
+        for (let i of dotai){
+          i.classList.remove("active");
+        };
+        elem.classList.add("active");
+    
+        let siblingsCount = sekcija.querySelectorAll(dotClass).length;
+        let nextSiblingsCount = 0;
+        let index = 0;
+        let nextSibling = elem.nextElementSibling;
+        while(nextSibling) {
+          nextSiblingsCount++;
+          nextSibling = nextSibling.nextElementSibling;
+        }
+        index = siblingsCount - nextSiblingsCount - 1;
+
+        let tesWidth = document.querySelector('.testimonials .slide2').offsetWidth;
+        document.querySelector('.'+n).style.setProperty(cssVariable, tesWidth);
+        document.querySelector('.'+n).style.setProperty("--testimonial-index", index);
+      };
+    
+
+
+
+
+
+
+
+
+
+
+
+
+export { changeHeight, observer, slider };
--- js/pages/home.js
+++ js/pages/home.js
@@ -1,6 +1,7 @@
 // IMPORTS
 
-
+import { changeHeight, observer, slider } from "../components/testimonials.js";
+import { carousel } from "../components/featured.js";
 // FUNCTION EXECUTION
 
 
@@ -25,38 +26,14 @@
 /* featured projects: start */
 
     //taskiukai violetiniai po skaidrem kad elgtusi kaip reikia
-      function testas(obj){
+      function dotsScroll(obj){
         let elem = obj.target;
-        dots3(elem, 'featured-projects')
+        carousel(elem, 'featured-projects', '.dot3', '--slide-number')
       };
     
-      function dots3(elem,n){
-        let sekcija = document.getElementById(n);
-        let dotai3 = sekcija.querySelectorAll('.dot3');
-        // let elem = this;
-        for (let i of dotai3){
-          i.classList.remove("active");
-        };
-        elem.classList.add("active");
-
-        let siblingsCount = sekcija.querySelectorAll('.dot3').length;
-        let nextSiblingsCount = 0;
-        let index = 0;
-        let nextSibling = elem.nextElementSibling;
-        while(nextSibling) {
-          nextSiblingsCount++;
-          nextSibling = nextSibling.nextElementSibling;
-        }
-        index = siblingsCount - nextSiblingsCount - 1
-
-        let viewportWidth = window.innerWidth;
-        const slideWidth = sekcija.querySelector('.slide').offsetWidth;
-        const x = ((slideWidth + 10) * (4 + index) + (slideWidth / 2)) * (-1) + (viewportWidth / 2);
-        document.getElementById('slides-wrapper').style.transform = 'translateX(' + x + 'px)';   
-      };
       let obj = document.querySelectorAll('.dot3');
       for (let o of obj){
-      o.addEventListener("click", testas);
+      o.addEventListener("click", dotsScroll);
     }
 
 /* featured projects: end */
@@ -74,80 +51,24 @@
 /* got new project: end */
 
 /* testimonials: start */
-        //balloon aukstis funkcija kad balloonas butu nemaziau negu betkurio slido auksciui
-        function changeHeight(){
-          const slides = document.getElementsByClassName("slide2");
-          let heights = [];
-          for (let i of slides){
-              heights.push(parseInt(i.offsetHeight,10))
-          }
-          const height =(Math.max(...heights)+1)+'px';
-          document.getElementById("balloon").style.height = height;
+     
+      function dotsScroll2(obj){
+        let elem = obj.target;
+        slider(elem, 'testimonials', '.dot', '--testimonial-width')
       };
 
-      //iskvieciam funkcija changeHeight() 
+      let obj2 = document.querySelectorAll('.testimonials .dot');
+      for (let o of obj2){
+      o.addEventListener("click", dotsScroll2);
+      }
+
+      changeHeight();
       window.addEventListener("resize", changeHeight);
-      window.onresize = changeHeight();
+     
+    
 
-
-      //Scrolling slides navigation dots behavior 
-      let dotai = document.getElementsByClassName("dot");
-      let slideWidth = document.getElementsByClassName("slide2");
-
-      function dots(n){
-        for (let i of dotai){
-          i.classList.remove("active");
-        };
-        dotai[n].classList.add("active");
-
-        if (n===0){
-          document.getElementById("slides-container").style.transform = `translate(0px, 0px)`;
-        } else if(n===1){
-          document.getElementById("slides-container").style.transform = `translate(-${slideWidth[0].offsetWidth+20}px, 0px)`;
-        } else if (n===2){
-          document.getElementById("slides-container").style.transform = `translate(-${slideWidth[0].offsetWidth+slideWidth[1].offsetWidth+70}px, 0px)`;
-        }
-      };
-
-
-      //funkcija, kuri tikrina ar pakeitus ekrano dydi nereikia paskrlinti slido, kad visdar butu balloono veitoje
-      function resize2(){
-        let dotai2 = document.getElementsByClassName("dot");
-        let nth = -1;
-        for (let i=0; i<dotai2.length; i++){
-          if (dotai2[i].classList.contains('active')){
-            nth = i;
-            console.log(nth);
-          }
-        };
-
-          if (nth===0){
-            document.getElementById("slides-container").style.transform = `translate(0px, 0px)`;
-          } else if(nth===1){
-            document.getElementById("slides-container").style.transform = `translate(-${slideWidth[0].offsetWidth+20}px, 0px)`;
-          } else if (nth===2){
-            document.getElementById("slides-container").style.transform = `translate(-${slideWidth[0].offsetWidth+slideWidth[1].offsetWidth+70}px, 0px)`;
-          }
-     };
-      //iskvieciam funkcija, kad resizinus ekrane, pasikeistu ir skrollo vieta
-      window.addEventListener("resize", resize2);
-
-
-      //dots behavior ended
-
-    //apearing animation
-        const observer = new IntersectionObserver(entries => {
-            // Loop over the entries
-            entries.forEach(entry => {
-              // If the element is visible
-              if (entry.isIntersecting) {
-                // Add the animation class
-                entry.target.classList.add('animation');
-              }
-            });
-          });
-          
-        // observer.observe(document.querySelector('.headers-wrapper'));
+    // Tikrinam kai stebimi objektai ivaziuoja i ekrana, prasideda ju animacija  
+    
         observer.observe(document.querySelector('.animated'));
         observer.observe(document.querySelector('.animated2'));
     //apearing animation end
